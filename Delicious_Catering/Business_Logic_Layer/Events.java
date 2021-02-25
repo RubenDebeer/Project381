92ad9a2ae94fdc0bff698e241fcdad998b0704fa
RubenDebeer <boepsman@gmail.com>
2021-02-25 23:37:36 +0200

    Events_Class_Update2

--- Delicious_Catering/Business_Logic_Layer/Events.java
+++ Delicious_Catering/Business_Logic_Layer/Events.java
@@ -1,21 +1,27 @@
 package Delicious_Catering.Business_Logic_Layer;
 
+import java.security.PublicKey;
+
+import javax.swing.ButtonModel;
+
 public class Events {
     //Fields 
 
     private String Event_Name;
     private String Event_Type;
     private String Client_Name;
+    private String Client_SName;
     private int Event_Number;
     private String Decorations; 
     private int Number_Of_Adults;
     private int Number_Of_Kids;
     private int Booking_Number;
+    private String ClientPN;
 
     //Attributes 
 
         //Getters 
-        public String GetEvent(){
+        public String GetEventName(){
             return Event_Name;
         }
         public String GetEType(){
@@ -39,9 +45,53 @@ public class Events {
         public int GetBNum(){
             return Booking_Number;
         }
+        public String GetCSName(){
+            return Client_SName;
+        }
+        public String GetCPN(){ //Get the clients phone number 
+            return ClientPN;
+        }
 
         //Setters
+        public void SetEName( String EName){
+            this.Event_Name = EName;
+        }
 
+        public void SetEtype(String EType){
+            this.Event_Type = EType;
+        }
+
+        public void SetENumber(int ENumber ){
+            this.Event_Number = ENumber;
+        }
+
+        public void setCName(String CLName){
+            this.Client_Name = CLName;
+        }
+
+        public void SetCSName(String CLSName ){
+            this.Client_SName = CLSName;
+        }
+
+        public void SetCPN(String CLPN){
+            this.ClientPN = CLPN;
+        }
+
+        public void SetKidNum(int KidNum){
+            this.Number_Of_Kids = KidNum;
+        }
+
+        public void SetAudNum(int AudNum){
+            this.Number_Of_Adults = AudNum;
+        }
+
+        public void SetBNum(int BNumber){
+            this.Booking_Number = BNumber;
+        }
+
+        public void SetDecorations(String Decor){
+            this.Decorations = Decor;
+        }
 
     //Constructors 
 
@@ -49,5 +99,4 @@ public class Events {
 
     //Custom 
 
-    
 }
