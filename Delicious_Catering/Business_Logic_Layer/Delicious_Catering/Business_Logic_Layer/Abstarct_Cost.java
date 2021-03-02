174fb4b71952eab84f01233767b9cc683945dc21
RubenDebeer <boepsman@gmail.com>
2021-03-02 22:04:39 +0200

    Update_all

---
+++ Delicious_Catering/Business_Logic_Layer/Delicious_Catering/Business_Logic_Layer/Abstarct_Cost.java
@@ -0,0 +1,10 @@
+package Delicious_Catering.Business_Logic_Layer;
+
+public abstract class Abstarct_Cost {
+
+    protected String Type;
+    protected double Price;
+    protected int Quantity;
+
+    public abstract double Calculate_Cost();
+}
